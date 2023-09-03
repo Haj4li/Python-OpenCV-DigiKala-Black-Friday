0ddc1500d578fa663d91d5e3611825871c766854
Ali Nakhaee <alinakhaee78@gmail.com>
2023-09-03 11:01:06 +0330

    Add the Hunter

---
+++ hunter.py
@@ -0,0 +1,25 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+import random
+import cv2
+import shutil
+
+# من اسم اسکریپت رو هانتر گذاشتم فکر کنم بدونید واسه چی
+# this script written for treasure-hunt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
